6e5b4b83e146376f4360fd5a71fc6c969318940a
poker <liupengke@inagora.cn>
2019-05-21 18:17:12 +0800

    重构SidePanel，解决多次关闭时和dialog的层级关系错误问题

--- src/js/com/SidePanel.js
+++ src/js/com/SidePanel.js
@@ -1,85 +1,123 @@
-import SidePanel from './SidePanel.vue';
-let app = new Vue({
-	data:{
-		title: '',
-		visible: false,
-		html: '',
-		isPage: false
-	},
-	components: {SidePanel},
-	template: 
-		'<side-panel'
-			+ ' :visible="visible"'
-			+ ' :title="title"'
-			+ ' :html="html"'
-			+ ' :is-page="isPage"'
-			+ ' @close="hide">'
-		+ '</side-panel>',
-	methods: {
-		init(){
-			if(this.inited) return;
-			let el = document.createElement('div');
-			document.body.appendChild(el);
-			this.$mount(el);
-			this.inited = true;
-		},
-		showPage(url, title, options) {
-			this.init();
-			if(typeof title=='string') {
-				options = Object.assign(options||{}, {title});
-			} else {
-				options = title;
-			}
-			if(!options)
-				options = {};
-			this._options = options;
-			this.html = url;
-			this.visible = true;
-			this.isPage = true;
-			this.title = options.title||'';
-			if(this._options.listeners && this._options.listeners.show) {
-				this._options.listeners.show.call(this);
-			}
-		},
-		show(content, title, options){
-			this.init();
-			if(typeof title=='string') {
-				options = Object.assign(options||{}, {title});
-			} else {
-				options = title;
-			}
-			if(!options)
-				options = {};
-			if(/^https?:\/\//.test(content)){
-				this.showPage(content, options);
-			} else {
-				this._options = options;
-				this.html = content;
-				this.visible = true;
-				this.isPage = false;
-				this.title = options.title||'';
-				if(this._options.listeners && this._options.listeners.show) {
-					this._options.listeners.show.call(this);
+import XSidePanel from './SidePanel.vue';
+class SidePanel {
+	constructor(){
+		let self = this;
+		this.app = new Vue({
+			data:{
+				title: '',
+				visible: false,
+				html: '',
+				isPage: false
+			},
+			components: {XSidePanel},
+			template: 
+				'<x-side-panel'
+					+ ' :visible="visible"'
+					+ ' :title="title"'
+					+ ' :html="html"'
+					+ ' :is-page="isPage"'
+					+ ' @close="hide">'
+				+ '</x-side-panel>',
+			methods: {
+				hide(){
+					if(self == SidePanel._ins) {
+						SidePanel.hide();
+					} else 
+						self.hide();
 				}
 			}
-		},
-		hide() {
-			if(this._options.listeners && this._options.listeners.beforehide) {
-				let ret = this._options.listeners.beforehide.call(this);
-				if(ret===false)
-					return;
-			}
-
-			this.visible = false;
-			if(this.isPage) {
-				this.html = "about:blank";
-			} else {
-				this.html = '';
-			}
-			if(this._options.listeners && this._options.listeners.hide) {
-				this._options.listeners.hide.call(this);
+		});
+	}
+	init(){
+		if(this.inited) return;
+		let el = document.createElement('div');
+		document.body.appendChild(el);
+		this.app.$mount(el);
+		this.inited = true;
+	}
+	show(content, title, options){
+		this.init();
+		if(typeof title=='string') {
+			options = Object.assign(options||{}, {title});
+		} else {
+			options = title;
+		}
+		if(!options)
+			options = {};
+		if(/^https?:\/\//.test(content)){
+			this.showPage(content, options);
+		} else {
+			this._options = options;
+			this.app.html = content;
+			this.app.visible = true;
+			this.app.isPage = false;
+			this.app.title = options.title||'';
+			if(this._options.listeners && this._options.listeners.show) {
+				this._options.listeners.show.call(this.app);
 			}
 		}
 	}
-});
-export default app;
+	showPage(url, title, options) {
+		this.init();
+		if(typeof title=='string') {
+			options = Object.assign(options||{}, {title});
+		} else {
+			options = title;
+		}
+		if(!options)
+			options = {};
+		this._options = options;
+		this.app.html = url;
+		this.app.visible = true;
+		this.app.isPage = true;
+		this.app.title = options.title||'';
+		if(this._options.listeners && this._options.listeners.show) {
+			this._options.listeners.show.call(this.app);
+		}
+	}
+	hide(){
+		if(this._options.listeners && this._options.listeners.beforehide) {
+			let ret = this._options.listeners.beforehide.call(this.app);
+			if(ret===false)
+				return;
+		}
+
+		this.app.visible = false;
+		if(this.app.isPage) {
+			this.app.html = "about:blank";
+		} else {
+			this.app.html = '';
+		}
+		if(this._options.listeners && this._options.listeners.hide) {
+			this._options.listeners.hide.call(this.app);
+		}
+	}
+	destroy(){
+		this._options = null;
+		let el = this.app.$el;
+		this.app.$destroy();
+		el.remove();
+		this.app = null;
+	}
+
+	static show(content, title, options){
+		if(!this._ins){
+			this._ins = new SidePanel();
+		}
+		this._ins.show(content, title, options);
+	}
+	static showPage(url, title, options) {
+		if(!this._ins) {
+			this._ins = new SidePanel();
+		}
+		this._ins.showPage(url, title, options);
+	}
+	static hide(){
+		if(this._ins){
+			this._ins.hide();
+			this._ins.destroy();
+		}
+		this._ins = null;
+	}
+}
+export default SidePanel;
